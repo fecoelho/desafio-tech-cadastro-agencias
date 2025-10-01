51ec88ec272546629cc1c888c352f049f584fa91
fecoelho <fecoel@outlook.com>
2025-09-30 23:07:13 -0300

    feat: spring init

---
+++ src/main/java/com/fecoelho/desafio_tech_cadastro_agencias/DesafioTechCadastroAgenciasApplication.java
@@ -0,0 +1,13 @@
+package com.fecoelho.desafio_tech_cadastro_agencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTechCadastroAgenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTechCadastroAgenciasApplication.class, args);
+	}
+
+}
